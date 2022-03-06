5435d38b3c402e4dc714efdd5bbcbd010ff69ba2
Milan-code-person <91362767+Milan-code-person@users.noreply.github.com>
2022-03-06 11:58:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song=""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist =0;
 function preload(){
-    song.loadSound("music.mp3");
+    song = loadSound("music.mp3");
 }
 
 function setup(){
@@ -9,10 +14,49 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
+
+function modelLoaded(){
+    console.log('PoseNet is Iniatalized')
+}
+
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreLeftWrist > 0.2){
+        
+    circle(leftWristX,leftWristY);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume ="+volume;
+    song.setVolume(volume);
+    }
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        scoreLeftWrist  = results[0].pose.keypoints[9].score
+        console.log("scoreLeftWrist ="+scoreLeftWrist   )
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("lefWristX =" + leftWristX +"leftWristY =" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX +"rightWristY ="+rightWristY);
+    }
 }
